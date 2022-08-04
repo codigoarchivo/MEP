17142378a9d25c9005a8a729d8bea5a6946f5436
jackson jose quintero angulo <codigoarchivo@gmail.com>
2022-08-04 16:47:50 -0400

    cambio prueba

--- components/layout/ShopLayout.js
+++ components/layout/ShopLayout.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 
 import { AuthStateChange } from "../../helpers/AuthStateChange";
 
@@ -8,28 +8,28 @@ import Head from "next/head";
 
 import { WithSubnavigation } from "./nav/WithSubnavigation";
 
-import { logout } from "../../actions/auth";
-
 import { Footer } from "./foo/Footer";
 
-import { Toast } from "../../helpers/Toast";
+// import { logout } from "../../actions/auth";
+
+// import { Toast } from "../../helpers/Toast";
 
 const ShopLayout = ({ children, title }) => {
-  // useSelector
-  const { activeSelect: a = {} } = useSelector(({ auth }) => auth);
-  // dispatch
-  const dispatch = useDispatch();
+  // // useSelector
+  // const { activeSelect: a = {} } = useSelector(({ auth }) => auth);
+  // // dispatch
+  // const dispatch = useDispatch();
 
-  const err = locale === "en" ? en.error : es.error;
+  // const err = locale === "en" ? en.error : es.error;
 
-  useEffect(() => {
-    if (a?.emailVerified === false) {
+  // useEffect(() => {
+  //   if (a?.emailVerified === false) {
 
-      Toast("Tienes 60 segundos para revisar tu correo", "info", 5000)
-      
-      setTimeout(() => dispatch(logout(err)), 1000 * 60);
-    }
-  });
+  //     Toast("Tienes 60 segundos para revisar tu correo", "info", 5000)
+
+  //     setTimeout(() => dispatch(logout(err)), 1000 * 60);
+  //   }
+  // });
 
   const origin =
     (typeof window !== "undefined" && window.location.origin) || "";
