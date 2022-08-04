66cce4b9a0a1bd41d1361806aa08868429804868
jackson jose quintero angulo <codigoarchivo@gmail.com>
2022-08-04 16:26:49 -0400

    se agrego un cambio prueba

--- components/layout/ShopLayout.js
+++ components/layout/ShopLayout.js
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import { useRouter } from "next/router";
-
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { AuthStateChange } from "../../helpers/AuthStateChange";
 
@@ -10,17 +8,26 @@ import Head from "next/head";
 
 import { WithSubnavigation } from "./nav/WithSubnavigation";
 
+import { logout } from "../../actions/auth";
+
 import { Footer } from "./foo/Footer";
+
+import { Toast } from "../../helpers/Toast";
 
 const ShopLayout = ({ children, title }) => {
   // useSelector
   const { activeSelect: a = {} } = useSelector(({ auth }) => auth);
+  // dispatch
+  const dispatch = useDispatch();
 
-  // const { push } = useRouter();
+  const err = locale === "en" ? en.error : es.error;
 
-  // if (a?.emailVerified === false) {
-  //   alert("No esta Verificador");
-  // }
+  useEffect(() => {
+    if (a?.emailVerified === false) {
+      Toast("Tienes 60 segundos para revisar tu correo", "info", 5000)
+      setTimeout(() => dispatch(logout(err)), 1000 * 60);
+    }
+  });
 
   const origin =
     (typeof window !== "undefined" && window.location.origin) || "";
