c0ce5698cc16432d77dc75f37e8b1555c55572e8
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-09-09 13:06:59 -0400

    pages/_app.js: agrege dos etiqueta div para el config del spiner

--- pages/_app.js
+++ pages/_app.js
@@ -8,29 +8,29 @@ import { store } from "../store";
 
 import { PersistGate } from "redux-persist/integration/react";
 
-import { Center, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../theme";
 
-import { Breakpoints } from "../helpers/Breakpoints";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
-  const { points25 } = Breakpoints();
+
   return (
-      <ChakraProvider theme={theme}>
-        <Provider store={store}>
-          <PersistGate
-            loading={
-              <Center h={"calc(100vh - 50px)"} fontSize={points25}>
-                loading
-              </Center>
-            }
-            persistor={persistStore(store)}
-          >
-            <Component {...pageProps} />
-          </PersistGate>
-        </Provider>
-      </ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate
+          loading={
+            <div className="box__content">
+              <div className="spinner"></div>
+            </div>
+          }
+          persistor={persistStore(store)}
+        >
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
   );
 };
 
