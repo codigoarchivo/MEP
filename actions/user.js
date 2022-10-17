e3a8da09e6f9601f3bd6b02835492111ad19f3d9
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-17 15:17:33 -0400

    actions/user.js: agrego accion agregar para testimonials

---
+++ actions/user.js
@@ -0,0 +1,22 @@
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
+import { Toast } from "../helpers/Toast";
+
+import { types } from "../type";
+
+export const testimonialsAdd = (data, uid, err) => {
+  return async (dispatch) => {
+    try {
+      await dispatch(addTestimonials(data));
+
+      await setDoc(doc(db, "coments", uid.toString()), data);
+    } catch (error) {
+      Toast(err, "error", 3000);
+    }
+  };
+};
+
+const addTestimonials = (data) => ({
+  type: types.userTestimonialsAdd,
+  payload: data,
+});
