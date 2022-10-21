4161f14dc29a093abf279e439ebf0faa9a122aa8
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-21 10:40:17 -0400

    pages/about/me.js: agrege getServerSideProps

--- pages/about/me.js
+++ pages/about/me.js
@@ -1,23 +1,46 @@
 import { Container, VStack } from "@chakra-ui/react";
 
+import PropTypes from "prop-types";
+
 import { ScreenAbout } from "../../components/about/ScreenAbout";
 
 import ShopLayout from "../../components/layout/ShopLayout";
 
 import { BlogEdgar } from "../../components/about/BlogEdgar";
 
-const About = () => {
+import { dbComents } from "../../data/dbAbout";
+
+export async function getServerSideProps(context) {
+  context.res.setHeader(
+    "Cache-Control",
+    "public, max-age=86400, must-revalidate"
+  );
+
+  const coments = await dbComents();
+
+  return {
+    props: {
+      coments,
+    },
+  };
+}
+
+const About = ({ coments }) => {
   return (
     <ShopLayout>
       <Container maxW={"container.xl"} py={32}>
         <VStack spacing={20}>
           <BlogEdgar />
 
-          <ScreenAbout />
+          <ScreenAbout coments={coments} />
         </VStack>
       </Container>
     </ShopLayout>
   );
+};
+
+About.propTypes = {
+  coments: PropTypes.array,
 };
 
 export default About;
