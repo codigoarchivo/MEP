32c607d9bfd46f00d7afa5c18f67b444dd92fe96
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-09 20:28:04 -0400

    components/home/Home.js: estructure el home

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,206 +1,70 @@
-import React from "react";
+import { useRouter } from "next/router";
 
-import {
-  Box,
-  Button,
-  Container,
-  Heading,
-  HStack,
-  Stack,
-  Text,
-  useBreakpointValue,
-  VStack,
-  chakra,
-} from "@chakra-ui/react";
-
-import Image from "next/image";
+import { Container, Heading, HStack, Stack, VStack } from "@chakra-ui/react";
 
 import PropTypes from "prop-types";
 
 import Marquee from "react-fast-marquee";
 
-import Carousel from "nuka-carousel";
-
 import { SerchScreen } from "../search/SerchScreen";
 
 import { Breakpoints } from "../../helpers/Breakpoints";
 
-import { NavLink } from "../../utils/Navlink";
+import { ScreenCarousel } from "./ScreenCarousel";
 
-import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { ScreenHero } from "./ScreenHero";
+import { CreateNewAcount } from "./CreateNewAcount";
 
-import { ModeColor } from "../../helpers/ModeColor";
+import { en } from "../../translations/en";
+import { es } from "../../translations/es";
 
-export const Home = ({ listData, latestCartSelect, locale, es, en }) => {
+export const Home = ({ listData, latestCartSelect }) => {
+  // useRouter
+  const { locale } = useRouter();
   // Breakpoints
-  const {
-    content5,
-    bordes,
-    displayOff1,
-    all1,
-    points25,
-    fondo,
-  } = Breakpoints();
-  // use Carousel
-  const variant = useBreakpointValue({ base: 1, sm: 2, md: 2, lg: 3, xl: 4 });
-
-  const valImgW = useBreakpointValue({ base: 250, md: 400, lg: 450 });
-
-  const valImgH = useBreakpointValue({ base: 300, md: 450, lg: 550 });
-
-  const { modelF } = ModeColor();
+  const { points25 } = Breakpoints();
 
   return (
     <Container maxW={"container.xs"} px={{ base: 1, sm: 4 }}>
-      <Stack
-        flexDirection={content5}
-        justifyContent={"center"}
-        alignItems={"center"}
-        spacing={10}
-      >
-        <VStack
-          w={all1}
-          border={bordes}
-          rounded={"lg"}
-          p={{ base: 6, md: 8 }}
-          boxShadow={"dark-lg"}
-          mb={{ base: 5, md: 0 }}
-          backgroundColor={fondo}
-        >
-          <Heading w={"full"} fontSize={points25} wordBreak={"break-word"}>
-            {locale === "en-US" ? en.home.hA : es.home.hA}
-          </Heading>
-          <Text
-            fontSize={["sx", "sm", "md", "lg", "xl"]}
-            wordBreak={"break-word"}
-          >
-            {locale === "en-US" ? en.home.hB : es.home.hB}
-          </Text>
-        </VStack>
-        <Image
-          src={"/img/primary.png"}
-          alt="Picture of the author"
-          layout="fixed"
-          width={valImgW}
-          height={valImgH}
-          priority={true}
-        />
-      </Stack>
-      <Stack
-        p={5}
-        w={"full"}
-        flexDirection={"row"}
-        backgroundColor={fondo}
-        alignItems={"center"}
-        spacing={0}
-        rounded={"lg"}
-        boxShadow={"lg"}
-      >
-        <Heading
-          color={modelF}
-          display={displayOff1}
-          textTransform={"capitalize"}
-          w={"full"}
-          mr={5}
-          size={"sm"}
-        >
-          {locale === "en-US" ? en.home.hC : es.home.hC}
-        </Heading>
-        <Box>
-          <NavLink
-            size={"sm"}
-            variant={"primary"}
-            name={locale === "en-US" ? en.create : es.create}
-            href={"/auth/create"}
-            rounded={"lg"}
-          />
-        </Box>
-      </Stack>
+      {/* hero */}
+      <ScreenHero />
+
+      {/* create account */}
+      <CreateNewAcount />
+
       <VStack spacing={10} mt={16}>
-        {listData[0] ? (
-          <Stack w={"full"} spacing={10}>
-            <Heading w={"full"} fontSize={points25}>
-              {locale === "en-US" ? en.home.hD : es.home.hD}
-            </Heading>
-            <HStack>
-              <Marquee gradient={false} style={{ height: "420px" }}>
-                {listData.map((data) => (
-                  <SerchScreen key={data.id} {...data} />
-                ))}
-              </Marquee>
-            </HStack>
-          </Stack>
-        ) : (
-          ""
-        )}
-
-        {!!latestCartSelect[2] ? (
-          <Stack w={"full"} spacing={0} justifyContent={"center"}>
-            <Heading w={"full"} size={"lg"} fontSize={points25} mb={10}>
-              {locale === "en-US" ? en.home.hE : es.home.hE}
-            </Heading>
-
-            <Carousel
-              easing="easeInOutElastic"
-              wrapAround={true}
-              slidesToScroll={3}
-              slidesToShow={variant}
-              cellSpacing={0}
-              slideWidth={0.75}
-              cellAlign={"center"}
-              defaultControlsConfig={{
-                nextButtonText: (
-                  <Button
-                    as={"div"}
-                    variant={"primary"}
-                    rounded={"full"}
-                    w={11}
-                    fontSize={"2xl"}
-                  >
-                    <ChevronRightIcon />
-                  </Button>
-                ),
-                prevButtonText: (
-                  <Button
-                    as={"div"}
-                    variant={"primary"}
-                    rounded={"full"}
-                    w={11}
-                    fontSize={"2xl"}
-                  >
-                    <ChevronLeftIcon />
-                  </Button>
-                ),
-                pagingDotsStyle: {
-                  fill: "transparent",
-                },
-                nextButtonStyle: {
-                  backgroundColor: "transparent",
-                },
-                prevButtonStyle: {
-                  backgroundColor: "transparent",
-                },
-              }}
-            >
-              {latestCartSelect.map((data) => (
-                <chakra.ul key={data.id}>
-                  <SerchScreen {...data} />
-                </chakra.ul>
+        {/* Marquee */}
+        <Stack display={listData[0] ? "flex" : "none"} w={"full"} spacing={10}>
+          <Heading w={"full"} fontSize={points25}>
+            {locale === "en-US" ? en.home.hD : es.home.hD}
+          </Heading>
+          <HStack>
+            <Marquee gradient={false} style={{ height: "420px" }}>
+              {listData.map((data) => (
+                <SerchScreen key={data.id} {...data} />
               ))}
-            </Carousel>
-          </Stack>
-        ) : (
-          ""
-        )}
+            </Marquee>
+          </HStack>
+        </Stack>
+
+        {/* Carousel */}
+        <Stack
+          display={!!latestCartSelect[2] ? "flex" : "none"}
+          w={"full"}
+          spacing={0}
+          justifyContent={"center"}
+        >
+          <Heading w={"full"} size={"lg"} fontSize={points25} mb={10}>
+            {locale === "en-US" ? en.home.hE : es.home.hE}
+          </Heading>
+          <ScreenCarousel latestCartSelect={latestCartSelect} />
+        </Stack>
       </VStack>
     </Container>
   );
 };
 
 PropTypes.Home = {
-  listData: PropTypes.array,
+  listData: PropTypes.array.isRequired,
   latestCartSelect: PropTypes.array,
-  locale: PropTypes.string,
-  es: PropTypes.object,
-  en: PropTypes.object,
 };
